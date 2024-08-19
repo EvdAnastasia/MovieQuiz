5e5a545125bb98fa0a4ed8ea84b96368eab0039a
EvdAnastasia <anastasia.evdokimovitch@yandex.ru>
2024-08-19 14:39:20 +0300

    Изменен протакол MostPopulMovies на Decodable и удален комментарий кода

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -10,49 +10,6 @@ final class QuestionFactory {
         self.moviesLoader = moviesLoader
         self.delegate = delegate
     }
-    
-    //    private let questions: [QuizQuestion] = [
-    //        .init(
-    //            image: "The Godfather",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: true),
-    //        .init(
-    //            image: "The Dark Knight",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: true),
-    //        .init(
-    //            image: "Kill Bill",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: true),
-    //        .init(
-    //            image: "The Avengers",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: true),
-    //        .init(
-    //            image: "Deadpool",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: true),
-    //        .init(
-    //            image: "The Green Knight",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: true),
-    //        .init(
-    //            image: "Old",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: false),
-    //        .init(
-    //            image: "The Ice Age Adventures of Buck Wild",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: false),
-    //        .init(
-    //            image: "Tesla",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: false),
-    //        .init(
-    //            image: "Vivarium",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: false)
-    //    ]
 }
 
 extension QuestionFactory: QuestionFactoryProtocol {
